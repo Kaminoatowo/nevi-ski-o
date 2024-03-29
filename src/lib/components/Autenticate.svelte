66ebd66b46ed6e0f5f5fff5da08d1f32a9fe208b
Kaminoatowo <j.cocomello@gmail.com>
2024-03-29 16:20:31 +0100

    edit autenticate

--- src/lib/components/Autenticate.svelte
+++ src/lib/components/Autenticate.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
     import { authHandlers } from "../../store/store";
+    import { GoogleIcon } from 'svelte-uicons/brands';
 </script>
 
 <main class="flex flex-col items-center justify-center">
-    <div class="mt-12">
-        <h1 class="text-2xl text-white font-bold">Login</h1>
+    <div class="mt-12 rounded-lg bg-secondary-500/10 px-4 py-2">
+        <h1 class="text-2xl text-white font-bold">Accedi</h1>
         <button on:click={authHandlers.loginWithGoogle} class="p-4 bg-white text-black rounded-lg mt-12 font-bold">
-            Sign in with Google
+            <GoogleIcon size="1.5x" class="mr-2 inline-block" />
+            Accedi con Google
         </button>
     </div>
 </main>
