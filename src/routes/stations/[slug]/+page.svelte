e119a4c4d2418f52e11bde55c78d56b121a8140d
Kaminoatowo <j.cocomello@gmail.com>
2024-05-05 20:48:57 +0200

    frontend forecast

--- src/routes/stations/[slug]/+page.svelte
+++ src/routes/stations/[slug]/+page.svelte
@@ -2,13 +2,16 @@
 	import { DayCloudy, DayRain, DaySunny, DayHail, DaySnow, DayWindy, Windy } from 'svelte-weather';
 	import { Thermometer } from 'svelte-weather';
 	import ForecastCard from '$lib/components/ForecastCard.svelte';
+	import HourlyForecastCard from '$lib/components/HourlyForecastCard.svelte';
+	import WeatherImage from '$lib/components/WeatherImage.svelte';
 
 	export let data;
 	const current = data.current;
-	const forecast = data.forecast.slice(0, 5);
+	const forecast = data.forecast;
+	const hourly = data.hourly;
 </script>
 
-<div class="w-2/3 mx-auto bg-surface-500 rounded-lg mt-5">
+<div class="sm:w-4/5 mx-auto bg-surface-500 sm:rounded-lg mt-5">
 	<p class="p-6">
 		<a href="/" class="underline underline-offset-2">Home</a>>
 		<a href="/stations" class="underline underline-offset-2">Stazioni</a>>
@@ -22,11 +25,7 @@
 		</h1>
 		<div class="w-1/2 mx-auto border rounded text-center m-5 inline-block bg-secondary-500">
 			<p>
-				{#if Number(current.precipitationProb) > 20}
-				<DayRain class="inline" color="gray"/>
-				{:else}
-				<DaySunny class="inline" color="orange"/>
-				{/if}
+				<WeatherImage forecast={current} />
 				{current.conditions}
 				
 				{#if Number(current.temp) > 20}
@@ -42,28 +41,11 @@
 		
 	</div>
 
-	<ForecastCard {forecast} />
-
-	<ForecastCard {forecast}>
-		<div class="rounded-t-lg border-b border-b-surface-500 bg-surface-800 p-5">
-			<h2 class="text-2xl">
-				Previsioni
-			</h2>
-		</div>
-		<div class="max-h-[500px] overflow-auto px-4 py-6">
-			<ul class="list">
-				{#each forecast as day}
-				<li class="!rounded-md">
-					<div class="grid w-full grid-cols-6 items-center gap-3 py-2">
-						<p class="text-xs text-surface-400 sm:text-sm">
-							{day.date}
-						</p>
-					</div>
-				</li>
-				{/each}
-			</ul>
-		</div>
-	</ForecastCard>
+	<div class="grid grid-cols-2 sm:gap-4">
+		<ForecastCard {forecast} />
+	
+		<HourlyForecastCard forecast={hourly} />
+	</div>
 
 	<br>
 </div>
