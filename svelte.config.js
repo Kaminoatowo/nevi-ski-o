45c7ac4a59685cd2959f5c707f713576852a20ff
Kaminoatowo <j.cocomello@gmail.com>
2024-05-13 21:11:01 +0200

    fix: revert to before trying deploy

--- svelte.config.js
+++ svelte.config.js
@@ -22,9 +22,9 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		//adapter: adapter(),
 		adapter: vercel(
-			{
+			/*{
 				out: 'build'
-			}
+			}*/
 		)
 	}
 };
