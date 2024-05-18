732396536c4d4a4c490737315172d99113f513be
Kaminoatowo <j.cocomello@gmail.com>
2024-05-18 18:55:38 +0200

    feat: add stations and forecast

---
+++ src/routes/stations/[slug]/+page.server.ts
@@ -0,0 +1,128 @@
+import * as cheerio from 'cheerio';
+import { stat } from 'fs';
+
+export const load = async (serverLoadEvent) => {
+    const { fetch, params } = serverLoadEvent;
+    const station = await import(`../${params.slug}.md`);
+    const { code, name, region, country } = station.metadata;
+
+    const title = `${name}`;
+    //const STAT_URL = `https://dati.meteotrentino.it/service.asmx/ultimiDatiStazione?codice=${code}`;
+    const API_KEY = stationsConfig.apiKey;
+    const STAT_URL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${name}%2C%20${region}%2C%20${country}?unitGroup=metric&key=${API_KEY}&contentType=json`;
+    
+    const response = await fetch(STAT_URL);
+    const stationsJSON = await response.json();
+
+    //console.log(stationsJSON.days[0].hours[0].temp);
+    const current = currentConditions(stationsJSON);
+
+    //console.log(daysAgoConditions(stationsJSON)[3]);
+    const forecast = daysAgoConditions(stationsJSON).slice(0, 5);
+
+    //console.log(hourlyConditions(stationsJSON)[23].time);
+    const hourly = hourlyConditions(stationsJSON);
+
+    return {
+        title,
+        current,
+        forecast,
+        hourly
+    }   
+}
+
+function currentConditions(json: any) {
+
+    const currentConditions = json.currentConditions;
+
+    return {
+        temp: currentConditions.temp,
+        feelslike: currentConditions.feelslike,
+        humidity: currentConditions.humidity,
+        dew: currentConditions.dew,
+        precipitation: currentConditions.precip,
+        precipitationProb: currentConditions.precipprob,
+        snow: currentConditions.snow,
+        snowDepth: currentConditions.snowdepth,
+        precipitationType: currentConditions.preciptype,
+        windGust: currentConditions.windgust,
+        windSpeed: currentConditions.windspeed,
+        windDirection: currentConditions.winddir,
+        pressure: currentConditions.pressure,
+        visibility: currentConditions.visibility,
+        cloudCover: currentConditions.cloudcover,
+        solarRadiation: currentConditions.solarradiation,
+        solarEnergy: currentConditions.solarenergy,
+        uv: currentConditions.uv,
+        severeRisk: currentConditions.severerisk,
+        conditions: currentConditions.conditions,
+        icon: currentConditions.icon,
+        sunrise: currentConditions.sunrise,
+        sunset: currentConditions.sunset,
+        moonPhase: currentConditions.moonphase
+    }
+}
+
+function daysAgoConditions(json: any) {
+    const weekDays = ['Dom', 'Lun', 'Mar', 'Mer', 'Gio', 'Ven', 'Sab'];
+    const days = json.days;
+    const daysAgo = days.map((day: any) => {
+        return {
+            date: day.datetime,
+            day: weekDays[new Date(day.datetime).getDay()],
+            tempMax: day.tempmax,
+            tempMin: day.tempmin,
+            feelslikeMax: day.feelslikemax,
+            feelslikeMin: day.feelslikemin,
+            precip: day.precip,
+            precipProb: day.precipprob,
+            snow: day.snow,
+            snowDepth: day.snowdepth,
+            precipType: day.preciptype,
+            windGust: day.windgust,
+            windSpeed: day.windspeed,
+            windDirection: day.winddir,
+            pressure: day.pressure,
+            humidity: day.humidity,
+            dew: day.dew,
+            cloudCover: day.cloudcover,
+            conditions: day.conditions,
+            icon: day.icon,
+            sunrise: day.sunrise,
+            sunset: day.sunset,
+            moonPhase: day.moonphase
+        }
+    });
+    return daysAgo;
+}
+
+function hourlyConditions(json: any) {
+    const directions = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW'];
+    const hours = json.days[0].hours;
+    const hourly = hours.map((hour: any) => {
+        return {
+            time: hour.datetime,
+            temp: hour.temp,
+            feelslike: hour.feelslike,
+            humidity: hour.humidity,
+            dew: hour.dew,
+            precip: hour.precip,
+            snow: hour.snow,
+            snowDepth: hour.snowdepth,
+            precipType: hour.preciptype,
+            windGust: hour.windgust,
+            windSpeed: hour.windspeed,
+            windDirection: hour.winddir,
+            windArrow: directions[Math.round(hour.winddir / 45) % 8],
+            pressure: hour.pressure,
+            visibility: hour.visibility,
+            cloudCover: hour.cloudcover,
+            solarRadiation: hour.solarradiation,
+            solarEnergy: hour.solarenergy,
+            uv: hour.uvindex,
+            conditions: hour.conditions,
+            icon: hour.icon
+        }
+    });
+    return hourly;
+}
---
+++ src/routes/stations/[slug]/+page.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+	import { DayCloudy, DayRain, DaySunny, DayHail, DaySnow, DayWindy, Windy } from 'svelte-weather';
+	import { Thermometer } from 'svelte-weather';
+	import ForecastCard from '$lib/components/ForecastCard.svelte';
+	import HourlyForecastCard from '$lib/components/HourlyForecastCard.svelte';
+	import WeatherImage from '$lib/components/WeatherImage.svelte';
+
+	export let data;
+	const current = data.current;
+	const forecast = data.forecast;
+	const hourly = data.hourly;
+</script>
+
+<div class="sm:w-4/5 mx-auto bg-surface-500 sm:rounded-lg mt-5">
+	<p class="p-6">
+		<a href="/" class="underline underline-offset-2">Home</a>>
+		<a href="/stations" class="underline underline-offset-2">Stazioni</a>>
+		{data.title}
+	</p>
+
+	<div class="flex flex-row justify-end">
+		
+		<h1 class="p-5 text-3xl inline-block">
+			{data.title}
+		</h1>
+		<div class="w-1/2 mx-auto border rounded text-center m-5 inline-block bg-secondary-500">
+			<p>
+				<WeatherImage forecast={current} />
+				{current.conditions}
+				
+				{#if Number(current.temp) > 20}
+				<Thermometer class="inline"	color="red"/>
+				{:else if Number(current.temp) < 10}
+				<Thermometer class="inline"	color="blue"/>
+				{:else}
+				<Thermometer class="inline"	color="gray"/>
+				{/if}
+				{current.temp}°C
+			</p>
+		</div>
+		
+	</div>
+
+	<div class="grid grid-cols-2 sm:gap-4">
+		<ForecastCard {forecast} />
+	
+		<HourlyForecastCard forecast={hourly} />
+	</div>
+
+	<br>
+</div>
