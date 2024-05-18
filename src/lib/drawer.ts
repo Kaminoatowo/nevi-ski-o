681af28b5a108cb5cfea50766e3df9f91e1621c3
Kaminoatowo <j.cocomello@gmail.com>
2024-05-18 18:58:34 +0200

    build: hide auth to hide api error

---
+++ src/lib/drawer.ts
@@ -0,0 +1,18 @@
+import { 
+    getDrawerStore,
+    type DrawerSettings, 
+    type DrawerStore,
+    initializeStores,
+ } from '@skeletonlabs/skeleton';
+
+initializeStores();
+const drawerStore = getDrawerStore();
+const settings: DrawerSettings = { width: 'w-[280px] md:w-[480px]' };
+function drawerOpen(): void {
+    drawerStore.open(settings);
+}
+const drawerClose = (): void => {
+    drawerStore.close();
+};
+
+export { drawerOpen, drawerClose, drawerStore, settings};
