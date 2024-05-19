d6e8625f3ab9fc2e3aa4e968709ceca48bcaf859
Kaminoatowo <j.cocomello@gmail.com>
2024-05-19 11:07:54 +0200

    security: hide sensible data using environment variables

---
+++ src/lib/stations-vcw.config.js
@@ -0,0 +1,4 @@
+export const stationsConfig = {
+    apiKey: import.meta.env.VITE_VCW_API,
+    statUrl: import.meta.env.VITE_VCW_URL,
+}
--- src/routes/stations/+page.svelte
+++ src/routes/stations/+page.svelte
@@ -1,10 +1,10 @@
 <script script>
-	export let data;
-	const stations = data.stations;
+	/*export let data;
+	const stations = data.stations;*/
 </script>
 
 <div class="w-1/2 mx-auto bg-surface-500 rounded-lg mt-5">
-	<h1 class="text-center h-20 p-5 text-2xl">Stazioni consultabili</h1>
+	<!--h1 class="text-center h-20 p-5 text-2xl">Stazioni consultabili</!--h1>
 	
 	{#each stations as station}
 		<div class="h-10 border rounded text-center p-1 m-4 bg-secondary-500 hover:bg-success-500">
@@ -15,5 +15,12 @@
 			</a>
 		</div>
 	{/each}
-	<br>
+	<br-->
+
+	<div class="">
+		<form action="get">
+			<input type="text" name="search" placeholder="Cerca stazione" class="w-1/2 p-2 m-2 rounded-lg text-black">
+			<button type="submit" class="bg-primary-500 p-2 m-2 rounded-lg">Cerca</button>
+		</form>
+	</div>
 </div>
