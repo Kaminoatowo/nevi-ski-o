7a4eab7cb0fd4d9b765dc7a73ded6935e468bfce
Kaminoatowo <j.cocomello@gmail.com>
2024-07-14 12:38:20 +0200

    fix style: home page resize

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,9 +5,9 @@
 
 <section class="flex flex-col items-center z-10">
     <img src={img} alt="NeviSki-o" class="w-64" />
-    <h1 class="text-4xl m-4">Ghe n'è de nef?</h1>
+    <h1>Non perderti la sciata perfetta!</h1>
     <!--p>Scopri se c'è neve sulle piste!</!--p-->
-    <p class="w-3/4 text-center text-xl m-4">Scopri il bollettino attuale sulla presenza di neve sulle tue piste preferite,
+    <p class="w-1/2 text-center text-xl m-4">Scopri il bollettino attuale sulla presenza di neve sulle tue piste preferite,
         ottieni previsioni accurate e ricevi notifiche in tempo reale.
     </p>
 </section>
